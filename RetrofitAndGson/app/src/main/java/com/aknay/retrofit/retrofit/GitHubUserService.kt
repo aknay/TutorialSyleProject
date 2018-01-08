7465ff2a6508e4728cbfe3000943a0542035bbb1
aknay <aknay@outlook.com>
2018-01-08 10:54:55 +0000

    added to wrong repo. removing

--- RetrofitAndGson/app/src/main/java/com/aknay/retrofit/retrofit/GitHubUserService.kt
+++
@@ -1,6 +0,0 @@
-package com.aknay.retrofit.retrofit
-
-object GitHubUserService {
-    val gitHubService: GitHubApiRoutes
-        get() = RetrofitClient.client.create(GitHubApiRoutes::class.java)
-}
